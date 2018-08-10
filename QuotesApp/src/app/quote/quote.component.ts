d04ef9e0767658e611250a4c4f26fba0937efee2
TonyKioko <tonykioko384@gmail.com>
2018-08-10 14:40:42 +0300

    generate quote component

---
+++ QuotesApp/src/app/quote/quote.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes = [
+
+    new Quote("Talk is cheap. Show me the code", "Linus", "Moringa Press"),
+    new Quote("I think [Arsene Wenger] is one of these people who is a voyeur. He likes to watch other people. There are some guys who, when they are at home, have a big telescope to see what happens in other families. He speaks, speaks, speaks about Chelsea.", "Jose Mourinho", "Moringa Press")
+
+  ]
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
