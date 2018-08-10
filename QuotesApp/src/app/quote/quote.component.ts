cf70342602fd36b032d77effc09da15ba0545cdd
TonyKioko <tonykioko384@gmail.com>
2018-08-10 16:18:24 +0300

    confirm delete message added

--- QuotesApp/src/app/quote/quote.component.ts
+++ QuotesApp/src/app/quote/quote.component.ts
@@ -10,16 +10,21 @@ export class QuoteComponent implements OnInit {
 
   quotes = [
 
-    new Quote(1,"Talk is cheap. Show me the code", "Linus", "Moringa Press"),
-    new Quote(2,"I think [Arsene Wenger] is one of these people who is a voyeur. He likes to watch other people. There are some guys who, when they are at home, have a big telescope to see what happens in other families. He speaks, speaks, speaks about Chelsea.", "Jose Mourinho", "Moringa Press")
+    new Quote(1, "Talk is cheap. Show me the code", "Linus", "Moringa Press"),
+    new Quote(2, "I think [Arsene Wenger] is one of these people who is a voyeur. He likes to watch other people. There are some guys who, when they are at home, have a big telescope to see what happens in other families. He speaks, speaks, speaks about Chelsea.", "Jose Mourinho", "Moringa Press")
 
   ]
   toogleDetails(index) {
     this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
   }
-  completeGoal(isComplete, index) {
+
+  deleteGoal(isComplete, index) {
     if (isComplete) {
-      this.quotes.splice(index, 1);
+      let toDelete = confirm(`Are you sure you want to delete this lovely quote -- ${this.quotes[index].quote}`)
+
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
     }
   }
 
